eb270dcb2ffd14f90d54a386d00be2b7cbd73c22
amrinarosidaa <amrinarosidaa6@gmail.com>
2024-12-24 09:20:17 +0700

    Pesan commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("demo").innerHTML = "Belajar Inner HTML";
+document.write("Ini hasil dari Document.write JS");
+window.alert("Ini Alert dari JS");
+
+console.log("Ini hasil Console Log");
+console.info("Ini hasil Console Info");
+console.warn("Ini hasil Console Warning");
+console.error("Ini hasil Console Eror");
+
+const dataTable = [
+    {id: 1, name: "amrina"},
+    {id: 2, name: "rosida"},
+];
+
+console.table(dataTable);
+console.info(dataTable);
